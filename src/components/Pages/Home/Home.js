71caaa79fc7d2518fe5aac7add252fc0579210bb
khanshorif331 <khanshorif331@gmail.com>
2022-05-03 01:04:19 +0600

    made a header

---
+++ src/components/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Items from './Items/Items'
+
+const Home = () => {
+	return (
+		<div>
+			<Items></Items>
+		</div>
+	)
+}
+
+export default Home
---
+++ src/components/RequireAuth/RequireAuth.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RequireAuth = () => {
+	return (
+		<div>
+			<h1>This is require auth page</h1>
+		</div>
+	)
+}
+
+export default RequireAuth
