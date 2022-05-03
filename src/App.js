0d52d0b4731ecacb0cd9c0361d37930eb8856f67
khanshorif331 <khanshorif331@gmail.com>
2022-05-04 01:18:34 +0600

    added many components

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './components/Pages/Home/Home'
 import Header from './components/Shared/Header/Header'
 import Footer from './components/Shared/Footer/Footer'
+import NotFound from './components/Shared/NotFound/NotFound'
 
 function App() {
 	return (
-		<div className='App'>
+		<div>
 			<Header></Header>
 			<Routes>
 				<Route path='/' element={<Home></Home>}></Route>
+				<Route path='/home' element={<Home></Home>}></Route>
+				<Route path='*' element={<NotFound></NotFound>}></Route>
 			</Routes>
 			<Footer></Footer>
-			{/* <h1>Hello World</h1> */}
 		</div>
 	)
 }
---
+++ src/components/Shared/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NotFound = () => {
+	return (
+		<div className='w-full mx-auto'>
+			{/* <h1>Page not found</h1> */}
+			<img
+				width='100%'
+				src='https://i.ytimg.com/vi/m7ZZNsa0pOA/maxresdefault.jpg'
+				alt=''
+			/>
+		</div>
+	)
+}
+
+export default NotFound
