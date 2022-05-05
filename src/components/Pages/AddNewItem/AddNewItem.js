f19db87dec9ce5d79286c80f4b2c61a918834b1c
khanshorif331 <khanshorif331@gmail.com>
2022-05-05 23:11:07 +0600

    added more functionality with add new item

--- src/components/Pages/AddNewItem/AddNewItem.js
+++ src/components/Pages/AddNewItem/AddNewItem.js
@@ -1,70 +1,78 @@
 import React from 'react'
 // import React from "react";
 import { useForm } from 'react-hook-form'
+import 'react-toastify/dist/ReactToastify.css'
 // import Header from './Header'
 import { useState } from 'react'
+import axios from 'axios'
+import { toast, ToastContainer } from 'react-toastify'
 
 const AddNewItem = () => {
-	const { register, handleSubmit } = useForm()
-	const [data, setData] = useState('')
+	const { register, handleSubmit, reset } = useForm()
+	const [item, setItem] = useState('')
 
-	const onSubmit = data => {
+	const onSubmit = async inputValue => {
+		// setItem(JSON.stringify(item))
+		setItem(inputValue)
+
+		const { data } = await axios.post('http://localhost:5000/items', item)
+		toast.success('Item added succesfully')
 		console.log(data)
-		setData(JSON.stringify(data))
-		console.log(data)
+		// reset()
 	}
 
 	return (
-		<div className='w-1/2 mx-auto'>
+		<div className='w-full md:w-1/2 mx-auto'>
 			<form
-				className='border-4 mx-auto w-50'
+				className='border-4 mx-auto w-full md:50 mt-10 rounded-lg'
 				onSubmit={handleSubmit(onSubmit)}
 			>
 				<input
 					{...register('name')}
 					placeholder='Product Name'
-					className='block mx-auto my-6 p-2 text-center'
+					className='block mx-auto my-6 p-2 w-3/4 rounded-lg text-center'
 					required
 				/>
 				<input
 					type='number'
 					{...register('price')}
 					placeholder='Price'
-					className='block border-0 mx-auto my-6 p-2 text-center'
+					className='block border-0 mx-auto my-6 p-2 text-center w-3/4 rounded-lg'
 					required
 				/>
 				<input
 					type='number'
 					{...register('quantity')}
 					placeholder='Quantity'
-					className='block border-0 mx-auto my-6 p-2 text-center'
+					className='block border-0 mx-auto my-6 p-2 text-center w-3/4 rounded-lg'
 					required
 				/>
 				<input
 					{...register('supplier')}
 					placeholder='Supplier'
-					className='block mx-auto my-6 p-2 text-center'
+					className='block mx-auto my-6 p-2 text-center w-3/4 rounded-lg'
 					required
 				/>
 				<input
 					{...register('img')}
 					placeholder='Image Link'
-					className='block mx-auto my-6 p-2 text-center'
+					className='block mx-auto my-6 p-2 text-center w-3/4 rounded-lg'
 					required
 				/>
 
 				<textarea
 					{...register('description')}
 					placeholder='Description'
-					className='block border-0 mx-auto my-6 p-2 text-center'
+					className='block border-0 mx-auto my-6 p-2 text-center w-3/4 rounded-lg'
 					required
 				/>
-				<p>{data}</p>
+				{/* <input type="submit" value="Submit" class="btn" /> */}
 				<input
 					type='submit'
-					className='block mx-auto my-6 p-2 text-center'
+					className='btn block mx-auto my-6 p-2 text-center w-3/4 rounded-lg cursor-pointer'
 				/>
 			</form>
+			<ToastContainer></ToastContainer>
 		</div>
 	)
 }
