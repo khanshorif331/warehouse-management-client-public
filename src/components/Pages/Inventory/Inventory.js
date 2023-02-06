12ac7bf973b3c8443660095de5bc3b0acc91a49f
khanshorif331 <khanshorif331@gmail.com>
2023-02-06 23:33:21 +0600

    some padding inthe inventory to fix the bug

--- src/components/Pages/Inventory/Inventory.js
+++ src/components/Pages/Inventory/Inventory.js
@@ -35,8 +35,8 @@ const Inventory = () => {
 	}
 
 	return (
-		<div className="mx-auto">
-			<h1 className="text-xl text-yellow-500 text-center mt-9">
+		<div className="mx-auto fixed">
+			<h1 className="text-xl text-yellow-500 text-center my-9 pt-4">
 				Inventory Items
 			</h1>
 
